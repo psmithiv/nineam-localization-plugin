11eea71acd52732e65501531f41dba0163696533
psmithiv <paul.smith.iv@ninthavenuemedia.com>
2013-07-24 12:29:28 -0400

    add touch-2.1.1 example

---
+++ examples/touch-2.2.1-example/locale/touch-lang-en.js
@@ -0,0 +1,9 @@
+Ext.onReady(function() {
+    Ext.define("Touch.locales.en.Global", {
+        title: 'en.title',
+        btnWidth: 300,
+        comp: {
+            title: 'en.comp.title'
+        }
+    });
+});
---
+++ examples/touch-2.2.1-example/locale/touch-lang-es.js
@@ -0,0 +1,9 @@
+Ext.onReady(function() {
+    Ext.define("Touch.locales.es.Global", {
+        title: 'es.title',
+        btnWidth: 100,
+        comp: {
+            title: 'es.comp.title'
+        }
+    });
+});
